87ebcfa709fe67cbd25f09e4bdd898f202368a6d
egushan <git.xports.cn>
2023-11-21 15:26:51 +0800

    first commit

--- src/views/upload/index.vue
+++ src/views/upload/index.vue
@@ -16,76 +16,50 @@
       <div class="el-upload__tip">jpg/png files with a size less than 500kb</div>
     </template>
   </el-upload>
-  {{ uploadSpeedKb }}
+  {{ speedInfoRef }} {{ leftTimeRef }} {{ progressBarRef }}
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
-  name: 'uploadPage',
-  data() {
-    return {
-      linkSpeedTimer: null,
-      uploadSpeedKb: 0,
-      uploadSpeed: 0,
-      progressLoad: 0,
-      fileSizeMb: 0,
-      progressPercent: 0
-    }
-  },
-  mounted() {
-    this.onConnectionChange()
-  },
-  computed: {
-    planTimes() {
-      const { uploadSpeedKb, fileSizeMb } = this
-      const remainPackSize =
-        (fileSizeMb - parseInt((this.progressPercent / 100) * fileSizeMb, 10)) * 1024
-      const remainTime = parseInt(remainPackSize / uploadSpeedKb, 10) // 秒
-      if (remainPackSize === 0 && this.progressPercent !== 100) {
-        return `1秒`
-      }
-      if (remainTime < 60) {
-        return `${remainTime}秒`
-      }
-      // eslint-disable-next-line camelcase
-      const min_total = Math.floor(remainTime / 60) || 0 // 分钟
-      const sec = Math.floor(remainTime % 60) || 0 // 余秒
-      // eslint-disable-next-line camelcase
-      if (min_total < 60) {
-        // eslint-disable-next-line camelcase
-        return `${min_total}分钟${sec}秒`
-      }
-      // eslint-disable-next-line camelcase
-      const hour_total = Math.floor(min_total / 60) || 0 // 小时数
-      // eslint-disable-next-line camelcase
-      const min = Math.floor(min_total % 60) || 0 // 余分钟
-      // eslint-disable-next-line camelcase
-      return `${hour_total}小时${min}分钟${sec}秒`
-    }
-  },
-  methods: {
-    handleChange(file) {
-      this.fileSizeMb = Number.parseInt(file.size / 1024 / 1024, 10)
-    },
-    onConnectionChange() {
-      this.linkSpeedTimer = setInterval(() => {
-        this.uploadSpeedKb = (navigator.connection.downlink * 1024) / 8
-        this.uploadSpeed =
-          (navigator.connection.downlink * 1024) / 8 > 1024
-            ? `${(navigator.connection.downlink * 1024) / 8 / 1024}m/s`
-            : `${(navigator.connection.downlink * 1024) / 8}kb/s`
-        if (!this.progressLoad) {
-          clearInterval(this.linkSpeedTimer)
-        }
-      }, 1000)
-    },
-    handleProgress(e) {
-      this.progressPercent = e.percent
-    },
-    handleSuccess() {
-      this.progressPercent = 100
-    }
-  }
+  name: 'uploadPage'
+}
+</script>
+
+<script setup>
+const startTimeRef = ref(Date.now())
+
+const handleChange = () => {
+  startTimeRef.value = Date.now()
+}
+
+const speedInfoRef = ref('')
+const leftTimeRef = ref('')
+const progressBarRef = ref('')
+
+const handleProgress = event => {
+  const uploaded = event.loaded
+  const { total } = event
+
+  // 计算上传进度百分比
+  progressBarRef.value = event.percent
+
+  // 计算上传速度
+  const deltaTime = (Date.now() - startTimeRef.value) / 1000 // 单位: 秒
+  const speed = uploaded / deltaTime / (1024 * 1024) // 单位: MB/s
+
+  speedInfoRef.value = `${speed.toFixed(2)} MB/s`
+
+  // 计算上传剩余时间
+  const leftTime = (total - uploaded) / (speed * 1024 * 1024) // 单位: 秒
+  leftTimeRef.value = `${leftTime.toFixed(0)} 秒`
+}
+
+const handleSuccess = () => {
+  speedInfoRef.value = `0 MB/s`
+  leftTimeRef.value = `0 秒`
+  progressBarRef.value = `100`
 }
 </script>
 
