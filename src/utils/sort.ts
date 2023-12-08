154bfcb99b4356b05d87fe619d96453b2ff05ed7
egushan <git.xports.cn>
2023-12-08 15:02:46 +0800

    first commit

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 function parallel(...args) {
   const len = Math.max(...args.map(arg => arg.length))
   const result = []
