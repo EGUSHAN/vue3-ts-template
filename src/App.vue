06deb0328a0f96b5c302676cd38c144b3360eab8
egushan <git.xports.cn>
2023-12-08 14:54:52 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,12 @@
 
 <script lang="ts">
 import { computed, onMounted } from 'vue'
+// 针对 element-plus 的中英文
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import en from 'element-plus/dist/locale/en.mjs'
+// emitter 组件通信
 import emitter from '@/utils/emitter.ts'
+// 系统状态管理参数
 import { useSystemStore } from '@/store/system.ts'
 import useThemeByOs from '@/hooks/useThemeByOs.ts'
 
